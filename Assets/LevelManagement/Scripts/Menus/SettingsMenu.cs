1cfe6133ae67bac0be2f285dab9b12501bf9f15a
DutchRascal <DutchRascal@fritz.box>
2020-02-17 16:57:06 +0100

    LM-036

--- Assets/LevelManagement/Scripts/Menus/SettingsMenu.cs
+++ Assets/LevelManagement/Scripts/Menus/SettingsMenu.cs
@@ -1,21 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace LevelManagement
 {
     public class SettingsMenu : Menu<SettingsMenu>
     {
+        [SerializeField] Slider _masterVolumeSlider;
+        [SerializeField] Slider _sfxVolumeSlider;
+        [SerializeField] Slider _musicVolumeSlider;
+
+
+        protected override void Awake()
+        {
+            base.Awake();
+            LoadPreferences();
+        }
+
         public void OnMasterVolumeChanged(float volume)
         {
+            PlayerPrefs.SetFloat("MasterVolume", volume);
         }
 
         public void OnSFXVolumeChanged(float volume)
         {
+            PlayerPrefs.SetFloat("SFXVolume", volume);
         }
 
         public void OnMusicVolumeChanged(float volume)
         {
+            PlayerPrefs.SetFloat("MusicVolume", volume);
+        }
+
+        public override void OnBackPressed()
+        {
+            base.OnBackPressed();
+            PlayerPrefs.Save();
+        }
+
+        public void LoadPreferences()
+        {
+            _masterVolumeSlider.value = PlayerPrefs.GetFloat("MasterVolume");
+            _sfxVolumeSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+            _musicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolume");
         }
     }
 }
