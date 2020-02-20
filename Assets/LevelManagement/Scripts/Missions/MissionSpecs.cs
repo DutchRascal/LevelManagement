1b17e6f407b903b38724e34523b22cf8c2599ac5
DutchRascal <DutchRascal@fritz.box>
2020-02-20 13:43:06 +0100

    LM-045

---
+++ Assets/LevelManagement/Scripts/Missions/MissionSpecs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace LevelManagement.Missions
+{
+    [Serializable]
+    public class MissionSpecs
+    {
+        [SerializeField] protected string _name;
+        [SerializeField] protected string _description;
+        [SerializeField] protected string _sceneName;
+        [SerializeField] protected string _id;
+        [SerializeField] protected Sprite _image;
+
+        public string Name => _name;
+        public string Descriptionget => _description;
+        public string SceneName => _sceneName;
+        public string Id => _id;
+        public Sprite Image => _image;
+    }
+
+}
